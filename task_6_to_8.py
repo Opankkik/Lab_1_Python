5fac244fed18a5b2696377426c77efea6d5f822b
Алишер <Alisher2806sochi@gmail.com>
2026-04-12 18:41:44 +0300

    Задание 7: Минимальное рациональное число.

--- task_6_to_8.py
+++ task_6_to_8.py
@@ -4,10 +4,15 @@ def max_real_number(s):
     numbers = re.findall(r'-?\d+\.\d+|-?\d+', s)
     return max(map(float, numbers)) if numbers else None
 
+def min_rational_number(s):
+    numbers = re.findall(r'-?\d+\.\d+|-?\d+', s)
+    return min(map(float, numbers)) if numbers else None
+
 def main():
     while True:
-        c = input("\n1 - Макс. вещественное, 0 - Выход\nВыбор: ")
+        c = input("\n1 - Макс. веществ., 2 - Мин. рацион., 0 - Выход\nВыбор: ")
         if c == '1': print("Результат:", max_real_number(input("Строка: ")))
+        elif c == '2': print("Результат:", min_rational_number(input("Строка: ")))
         elif c == '0': break
 
 if __name__ == "__main__": main()
