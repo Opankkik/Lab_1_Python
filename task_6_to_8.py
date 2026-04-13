398073aaef8dba0281587419714c0f52bf7f36f1
Алишер <Alisher2806sochi@gmail.com>
2026-04-13 08:32:44 +0300

    Задание 8: Наибольшее количество подряд идущих цифр

--- task_6_to_8.py
+++ task_6_to_8.py
@@ -1,18 +1,23 @@
 import re
 
 def max_real_number(s):
-    numbers = re.findall(r'-?\d+\.\d+|-?\d+', s)
-    return max(map(float, numbers)) if numbers else None
+    nums = re.findall(r'-?\d+\.\d+|-?\d+', s)
+    return max(map(float, nums)) if nums else None
 
 def min_rational_number(s):
-    numbers = re.findall(r'-?\d+\.\d+|-?\d+', s)
-    return min(map(float, numbers)) if numbers else None
+    nums = re.findall(r'-?\d+\.\d+|-?\d+', s)
+    return min(map(float, nums)) if nums else None
+
+def max_consecutive_digits(s):
+    seqs = re.findall(r'\d+', s)
+    return max(len(seq) for seq in seqs) if seqs else 0
 
 def main():
     while True:
-        c = input("\n1 - Макс. веществ., 2 - Мин. рацион., 0 - Выход\nВыбор: ")
+        c = input("\n1 - Макс. веществ., 2 - Мин. рацион., 3 - Подряд цифры, 0 - Выход\nВыбор: ")
         if c == '1': print("Результат:", max_real_number(input("Строка: ")))
         elif c == '2': print("Результат:", min_rational_number(input("Строка: ")))
+        elif c == '3': print("Результат:", max_consecutive_digits(input("Строка: ")))
         elif c == '0': break
 
 if __name__ == "__main__": main()
